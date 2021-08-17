06ab70b7357927c33593178c7682d4964a399f03
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-08-17 19:51:07 +0200

    feat: iterating to create markers

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+const visitedPlaces = [
+    {
+        name: "Krivan",
+        lat: 49.1481947,
+        lng: 19.9588182 
+    }
+];
 function initMap() {
-  const myLatLng = { lat: -25.363, lng: 131.044 };
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: myLatLng,
-  });
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello World!",
-  });
+    const myLatLng = { lat: 49.1481947, lng: 19.9588182 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 10,
+        center: myLatLng,
+    });
+
+    for (const place of visitedPlaces){
+        new google.maps.Marker({
+            position: {
+                lat: place.lat,
+                lng: place.lng,
+            },
+            map,
+            label: place.name
+        });
+    };
 }
