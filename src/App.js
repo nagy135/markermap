51745637ec6d19161776964917a2725d9cf8107b
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-09-02 18:56:32 +0200

    feat: precise coordinates

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ export default function SimpleMap(){
     const visitedPlaces = [
         {
             name: "Sitno",
-            lat: 48.4023742,
-            lng: 18.8661295,
+            lat: 48.402543576540275,
+            lng: 18.877049945323773,
             altitude: 1009
         },
         {
             name: "Salatin",
-            lat: 48.9805536,
-            lng: 19.3449904,
+            lat: 48.98032903440475,
+            lng: 19.36288620314865,
             altitude: 1630
         },
 
