3b31241804cf706ad5bbde9fc422e325b7271bc6
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-09-03 20:52:13 +0200

    feat: material-ui left drawer + refactor map

--- src/App.js
+++ src/App.js
@@ -1,74 +1,98 @@
-import React from "react";
-import GoogleMapReact from 'google-map-react';
+ 
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import Container from '@material-ui/core/Container';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import './App.css';
 
-export default function SimpleMap(){
+import Map from './components/Map';
+// import InboxIcon from '@material-ui/icons/MoveToInbox';
+// import MailIcon from '@material-ui/icons/Mail';
 
-    const slovakiaCenter = { lat: 48.6670441, lng: 19.7785865 };
-    const visitedPlaces = [
-        {
-            name: "Sitno",
-            lat: 48.402543576540275,
-            lng: 18.877049945323773,
-            altitude: 1009
-        },
-        {
-            name: "Salatin",
-            lat: 48.98032903440475,
-            lng: 19.36288620314865,
-            altitude: 1630
-        },
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
 
-    ];
+export default function App() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
 
-    const defaultProps = {
-        center: {
-            lat: slovakiaCenter.lat,
-            lng: slovakiaCenter.lng
-        },
-        zoom: 8
-    };
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
 
-    const renderMarkers = (map, maps) => {
-        visitedPlaces.map((item) => {
-            new maps.Marker({
-                position: { lat: item.lat, lng: item.lng },
-                map,
-                title: item.name + ' (' + item.altitude + 'm. n. m.)',
-                label: item.name
-            });
-        });
-    };
+    setState({ ...state, [anchor]: open });
+  };
 
-    return (
-        // Important! Always set the container height explicitly
-        <div style={{ height: '100vh', width: '100%' }}>
-            <GoogleMapReact
-                bootstrapURLKeys={{
-                    key: "AIzaSyBEPCDqimEMFijE3Oo0w22qn6dh8ql2Zg4&" ,
-                    v: "beta"
-                }}
-                defaultCenter={defaultProps.center}
-                defaultZoom={defaultProps.zoom}
-                yesIWantToUseGoogleMapApiInternals
-                onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
-                // layerTypes={['TransitLayer', 'TrafficLayer']}
-                options={() => {
-                    return {
-                        panControl: true,
-                        tilt: 40.0,
-                        zoomControl: true,
-                        mapTypeControl: true,
-                        scaleControl: true,
-                        streetViewControl: true,
-                        rotateControl: true,
-                        fullscreenControl: true,
-                        scrollwheel: true,
+  const list = (anchor) => (
+    <div
+      className={clsx(classes.list, {
+        [classes.fullList]: anchor === 'top' || anchor === 'bottom',
+      })}
+      role="presentation"
+      onClick={toggleDrawer(anchor, false)}
+      onKeyDown={toggleDrawer(anchor, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon></ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
 
-
-                    } } }
-            >
-            </GoogleMapReact>
-        </div>
-    );
+  return (
+    <Container>
+        {['left'].map((anchor) => (
+            <div class="drawer">
+                <React.Fragment key={anchor}>
+                    <Button onClick={toggleDrawer(anchor, true)}>{anchor}</Button>
+                    <Drawer anchor={anchor} open={state[anchor]} onClose={toggleDrawer(anchor, false)}>
+                        {list(anchor)}
+                    </Drawer>
+                </React.Fragment>
+            </div>
+        ))}
+        <Map>
+        </Map>
+    </Container>
+  );
 }
+
+
+/////////////////////
+/////////////////////
+/////////////////////
+/////////////////////
+/////////////////////
+
---
+++ src/components/Map.js
@@ -0,0 +1,72 @@
+import GoogleMapReact from 'google-map-react';
+
+ export default function Map(){
+ 
+     const slovakiaCenter = { lat: 48.6670441, lng: 19.7785865 };
+     const visitedPlaces = [
+         {
+             name: "Sitno",
+             lat: 48.402543576540275,
+             lng: 18.877049945323773,
+             altitude: 1009
+         },
+         {
+             name: "Salatin",
+             lat: 48.98032903440475,
+             lng: 19.36288620314865,
+             altitude: 1630
+         },
+ 
+     ];
+ 
+     const defaultProps = {
+         center: {
+             lat: slovakiaCenter.lat,
+             lng: slovakiaCenter.lng
+         },
+         zoom: 8
+     };
+ 
+     const renderMarkers = (map, maps) => {
+         visitedPlaces.map((item) => {
+             new maps.Marker({
+                 position: { lat: item.lat, lng: item.lng },
+                 map,
+                 title: item.name + ' (' + item.altitude + 'm. n. m.)',
+                 label: item.name
+             });
+         });
+     };
+ 
+     return (
+         // Important! Always set the container height explicitly
+         <div style={{ height: '100vh', width: '100%' }}>
+             <GoogleMapReact
+                 bootstrapURLKeys={{
+                     key: "AIzaSyBEPCDqimEMFijE3Oo0w22qn6dh8ql2Zg4&" ,
+                     v: "beta"
+                 }}
+                 defaultCenter={defaultProps.center}
+                 defaultZoom={defaultProps.zoom}
+                 yesIWantToUseGoogleMapApiInternals
+                 onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+                 // layerTypes={['TransitLayer', 'TrafficLayer']}
+                 options={() => {
+                     return {
+                         panControl: true,
+                         tilt: 40.0,
+                         zoomControl: true,
+                         mapTypeControl: true,
+                         scaleControl: true,
+                         streetViewControl: true,
+                         rotateControl: true,
+                         fullscreenControl: true,
+                         scrollwheel: true,
+ 
+ 
+                     } } }
+             >
+             </GoogleMapReact>
+         </div>
+     );
+ }
