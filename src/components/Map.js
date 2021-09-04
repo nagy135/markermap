6dd808eb8c0a8d8c1139783a46b2c8f468182c52
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-09-04 18:33:40 +0200

    refactor: extract data to data files

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,29 +1,8 @@
 import GoogleMapReact from 'google-map-react';
 
+import { visited, slovakiaCenter } from '../resources/locations';
+
  export default function Map(){
- 
-     const slovakiaCenter = { lat: 48.6670441, lng: 19.7785865 };
-     const visitedPlaces = [
-         {
-             name: "Sitno",
-             lat: 48.402543576540275,
-             lng: 18.877049945323773,
-             altitude: 1009
-         },
-         {
-             name: "Salatin",
-             lat: 48.98032903440475,
-             lng: 19.36288620314865,
-             altitude: 1630
-         },
-         {
-             name: "Zaruby",
-             lat: 48.52358157633058,
-             lng: 17.39166116080486,
-             altitude: 768
-         },
- 
-     ];
  
      const defaultProps = {
          center: {
@@ -34,7 +13,7 @@ import GoogleMapReact from 'google-map-react';
      };
  
      const renderMarkers = (map, maps) => {
-         visitedPlaces.map((item) => {
+         visited.map((item) => {
              new maps.Marker({
                  position: { lat: item.lat, lng: item.lng },
                  map,
---
+++ src/resources/locations.js
@@ -0,0 +1,25 @@
+export const visited = [
+         {
+             name: "Sitno",
+             lat: 48.402543576540275,
+             lng: 18.877049945323773,
+             altitude: 1009
+         },
+         {
+             name: "Salatin",
+             lat: 48.98032903440475,
+             lng: 19.36288620314865,
+             altitude: 1630
+         },
+         {
+             name: "Zaruby",
+             lat: 48.52358157633058,
+             lng: 17.39166116080486,
+             altitude: 768
+         },
+];
+
+export const slovakiaCenter = {
+    lat: 48.6670441,
+    lng: 19.7785865
+};
