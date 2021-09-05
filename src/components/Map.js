00ae4932b5c549302d92dcbe501b4b3dc396a4b5
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-09-05 17:16:36 +0200

    feat: hook restriction + indentation fix

--- src/components/Map.js
+++ src/components/Map.js
@@ -6,65 +6,66 @@ import { visited, slovakiaCenter } from '../resources/locations';
 function Map(props){
 
     const [markers, setMarkers] = React.useState([]);
- 
-     const defaultProps = {
-         center: {
-             lat: slovakiaCenter.lat,
-             lng: slovakiaCenter.lng
-         },
-         zoom: 8
-     };
- 
-     const renderMarkers = (map, maps) => {
-         visited.map((item) => {
-             const marker = new maps.Marker({
-                 position: { lat: item.lat, lng: item.lng },
-                 map,
-                 title: item.name + ' (' + item.altitude + 'm. n. m.)',
-                 label: item.name
-             });
-             setMarkers(markers.push(marker));
-         });
-     };
 
-     const onAltitudesChange = (values) => {
-     };
+    const defaultProps = {
+        center: {
+            lat: slovakiaCenter.lat,
+            lng: slovakiaCenter.lng
+        },
+        zoom: 8
+    };
 
-     React.useEffect(()=> {
-         onAltitudesChange(props.altitudes);
-     });
- 
-     return (
-         // Important! Always set the container height explicitly
-         <div style={{ height: '100vh', width: '100%' }}>
-             <GoogleMapReact
-                 bootstrapURLKeys={{
-                     key: "AIzaSyBEPCDqimEMFijE3Oo0w22qn6dh8ql2Zg4&" ,
-                     v: "beta"
-                 }}
-                 defaultCenter={defaultProps.center}
-                 defaultZoom={defaultProps.zoom}
-                 yesIWantToUseGoogleMapApiInternals
-                 onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
-                 // layerTypes={['TransitLayer', 'TrafficLayer']}
-                 options={() => {
-                     return {
-                         panControl: true,
-                         tilt: 40.0,
-                         zoomControl: true,
-                         mapTypeControl: true,
-                         scaleControl: true,
-                         streetViewControl: true,
-                         rotateControl: true,
-                         fullscreenControl: true,
-                         scrollwheel: true,
- 
- 
-                     } } }
-             >
-             </GoogleMapReact>
-         </div>
-     );
- }
+    const renderMarkers = (map, maps) => {
+        visited.map((item) => {
+            const marker = new maps.Marker({
+                position: { lat: item.lat, lng: item.lng },
+                map,
+                title: item.name + ' (' + item.altitude + 'm. n. m.)',
+                label: item.name
+            });
+            setMarkers(markers.push(marker));
+        });
+    };
+
+    const onAltitudesChange = (values) => {
+        console.log('remove markers');
+    };
+
+    React.useEffect(()=> {
+        onAltitudesChange(props.altitudes);
+    }, [props.altitudes]);
+
+    return (
+        // Important! Always set the container height explicitly
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{
+                    key: "AIzaSyBEPCDqimEMFijE3Oo0w22qn6dh8ql2Zg4&" ,
+                    v: "beta"
+                }}
+                defaultCenter={defaultProps.center}
+                defaultZoom={defaultProps.zoom}
+                yesIWantToUseGoogleMapApiInternals
+                onGoogleApiLoaded={({ map, maps }) => renderMarkers(map, maps)}
+                // layerTypes={['TransitLayer', 'TrafficLayer']}
+                options={() => {
+                    return {
+                        panControl: true,
+                        tilt: 40.0,
+                        zoomControl: true,
+                        mapTypeControl: true,
+                        scaleControl: true,
+                        streetViewControl: true,
+                        rotateControl: true,
+                        fullscreenControl: true,
+                        scrollwheel: true,
+
+
+                    } } }
+            >
+            </GoogleMapReact>
+        </div>
+    );
+}
 
 export default Map;
