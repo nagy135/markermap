6beba6e66edd20861d5eb641dd43f49b29f235d0
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-03-18 12:12:12 +0100

    feat(api): supertokens

--- api/src/app.ts
+++ api/src/app.ts
@@ -6,6 +6,8 @@ import ErrorMiddleware from '@middleware/error.middleware';
 import Logger from '@handler/logger/winston';
 import RouterV1 from '@routes/v1';
 import config from '@config/config';
+import dotenv from 'dotenv';
+
 import {
   MorganErrorMiddleware,
   MorganInfoMiddleware,
@@ -71,18 +73,93 @@ const corsOptions: any = {
   ],
 };
 
+const Session = require('supertokens-node/recipe/session');
+// let { verifySession } = require("supertokens-node/recipe/session/framework/express");
+const {
+  middleware,
+  errorHandler,
+} = require('supertokens-node/framework/express');
+const ThirdPartyEmailPassword = require('supertokens-node/recipe/thirdpartyemailpassword');
+const supertokens = require('supertokens-node');
+
+const apiPort = 4200;
+const apiDomain = `http://localhost:${apiPort}`;
+
+const websitePort = 3300;
+const websiteDomain = `http://localhost:${websitePort}`;
+
 /**
  * Run function
  */
 const createApplication = async () => {
   try {
     const app: Application = Express();
+    supertokens.init({
+      framework: 'express',
+      supertokens: {
+        connectionURI: 'http://localhost:3567',
+      },
+      appInfo: {
+        appName: 'Marker map', // TODO: Your app name
+        apiDomain, // TODO: Change to your app's API domain
+        websiteDomain, // TODO: Change to your app's website domain
+      },
+      recipeList: [
+        ThirdPartyEmailPassword.init({
+          providers: [
+            // We have provided you with development keys which you can use for testing.
+            // IMPORTANT: Please replace them with your own OAuth keys for production use.
+            ThirdPartyEmailPassword.Google({
+              clientId:
+                '1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com',
+              clientSecret: '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',
+            }),
+            ThirdPartyEmailPassword.Github({
+              clientSecret: 'e97051221f4b6426e8fe8d51486396703012f5bd',
+              clientId: '467101b197249757c71f',
+            }),
+          ],
+        }),
+        Session.init(),
+      ],
+    });
+
+    app.use(
+      cors({
+        ...corsOptions,
+        allowedHeaders: [
+          ...corsOptions.allowedHeaders,
+          ...supertokens.getAllCORSHeaders(),
+        ],
+        methods: ['GET', 'PUT', 'POST', 'DELETE'],
+        credentials: true,
+      })
+    );
+
+    // app.use(
+    //   helmet({
+    //     contentSecurityPolicy: false,
+    //   })
+    // );
+    app.use(middleware());
+
+    // // custom API that requires session verification
+    // app.get("/sessioninfo", verifySession(), async (req, res) => {
+    //   let session = req.session;
+    //   res.send({
+    //     sessionHandle: session.getHandle(),
+    //     userId: session.getUserId(),
+    //     accessTokenPayload: session.getAccessTokenPayload(),
+    //   });
+    // });
+
+    app.use(errorHandler());
+
     app.use(Express.static('uploads'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(MorganInfoMiddleware);
     app.use(MorganErrorMiddleware);
-    app.use(cors(corsOptions));
     app.use('/v1', RouterV1);
     app.use(ErrorMiddleware);
     const server: Net.Server = app.listen(config.port);
--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -6,10 +6,10 @@ export interface IConfig {
 }
 
 const config: IConfig = {
-  service: 'api-gateway',
+  service: 'api',
   userRole: 'cbae4f81-a1df-4ef3-9a48-9ad28a21af3a',
   adminRole: '7c70e397-45c0-47e7-a5f1-b220b21fe55d',
-  port: 7000,
+  port: 4200,
 };
 
 export default config;
