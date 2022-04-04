c1a1648e28bcddc46745ce1cfbcf91acc89a6081
Viktor Nagy <viktor.nagy1995@gmail.com>
2022-04-04 14:26:57 +0200

    feat(app): working upload demo (with multi upload)

--- app/src/components/adder.tsx
+++ app/src/components/adder.tsx
@@ -1,7 +1,8 @@
-import { Box, Button, Container, Input, Stack, TextField } from "@mui/material";
+import { Box, Button, Container, Stack, TextField } from "@mui/material";
 import axios from "axios";
 import { useState } from "react";
 import "./css/adder.css";
+import { toast } from "../utils/toast";
 
 import { useSessionContext } from "supertokens-auth-react/recipe/session";
 import { useSearchParams } from "react-router-dom";
@@ -12,48 +13,50 @@ export default function Detail(_props: any) {
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
-  const [uploadedFile, setUploadedFile] = useState<any>({});
-  const [recordName] = useState("");
-  const [recordLat] = useState(lat ?? "");
-  const [recordLng] = useState(lng ?? "");
+  const [recordId, setRecordId] = useState<string | null>(null);
+  const [recordName, setRecordName] = useState("");
+  const [recordLat, setRecordLat] = useState(lat ?? "");
+  const [recordLng, setRecordLng] = useState(lng ?? "");
+  const [recordAltitude, setRecordAltitude] = useState("");
 
-  const onFileChange = (event: any) => {
-    setUploadedFile({ selectedFile: event.target.files[0] });
-  };
-
-  const onFileUpload = async () => {
+  /**
+   * uploads image, can be called multiple times but only first call creates record
+   *
+   * @author Viktor Nagy <viktor.nagy@01people.com>
+   */
+  const onFileChange = async (event: any) => {
+    const uploadedFile = event.target.files[0];
     const formData = new FormData();
 
     formData.append(
       "image", // NOTE: REQUIRED FORM DATA NAME! validated at api
-      uploadedFile.selectedFile,
-      uploadedFile.selectedFile.name
+      uploadedFile,
+      uploadedFile.name
     );
 
     const endpoint = "http://localhost:4200/v1";
 
-    const recordUploadResponse = (
-      await axios.post(`${endpoint}/records`, {
-        userId,
-        name: "TEST",
-        lat: (Number("49.18382677567287") + Math.random() / 10).toFixed(14),
-        lon: (Number("19.752156530646067") + Math.random() / 10).toFixed(14),
-        altitude: 2200,
-      })
-    ).data?.data;
+    if (recordId) {
+      formData.append("recordId", recordId);
+    } else {
+      const recordUploadResponse = (
+        await axios.post(`${endpoint}/records`, {
+          userId,
+          name: recordName,
+          lat: Number(recordLat).toFixed(14),
+          lon: Number(recordLng).toFixed(14),
+          altitude: Number(recordAltitude),
+        })
+      ).data?.data;
 
-    formData.append("recordId", recordUploadResponse.recordId);
+      setRecordId(recordUploadResponse.recordId);
+      toast("Record created!");
 
-    const imageUploadResponse = await axios.post(
-      `${endpoint}/images`,
-      formData
-    );
-    console.log(
-      "================\n",
-      "imageUploadResponse: ",
-      imageUploadResponse,
-      "\n================"
-    );
+      formData.append("recordId", recordUploadResponse.recordId);
+    }
+
+    await axios.post(`${endpoint}/images`, formData);
+    toast("Image uploaded!");
   };
 
   return (
@@ -61,15 +64,32 @@ export default function Detail(_props: any) {
       <h1>Create New Record</h1>
       <Box className="create-record">
         <Stack spacing={2}>
-          <TextField label="Name" value={recordName} />
-          <TextField label="Latitude" value={recordLat} />
-          <TextField label="Longitude" value={recordLng} />
+          <TextField
+            label="Name"
+            value={recordName}
+            onChange={(e) => setRecordName(e.target.value)}
+          />
+          <TextField
+            label="Latitude"
+            value={recordLat}
+            onChange={(e) => setRecordLat(e.target.value)}
+          />
+          <TextField
+            label="Longitude"
+            value={recordLng}
+            onChange={(e) => setRecordLng(e.target.value)}
+          />
+          <TextField
+            label="Altitude"
+            value={recordAltitude}
+            onChange={(e) => setRecordAltitude(e.target.value)}
+          />
+          <Box className="upload-info">
+            To upload multiple files, simply click "upload image" multiple times
+          </Box>
           <Button variant="contained" component="label">
             upload image
             <input type="file" hidden onChange={onFileChange} />
-          </Button>
-          <Button variant="contained" onClick={onFileUpload}>
-            Upload
           </Button>
         </Stack>
       </Box>
